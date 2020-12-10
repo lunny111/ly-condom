6ab1e9caf7073a672aae2dcbefac057d875bc14f
Lunny <69404314+lunny111@users.noreply.github.com>
2020-12-10 12:32:16 +0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Condom Script by Lunny#7190'
+
+version '1.0.0'
+
+server_scripts {
+	'server.lua'
+}
+
+client_scripts {
+	'client.lua'
+}
